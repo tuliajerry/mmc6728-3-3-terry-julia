af89dec4c6d7fca771f215f6d327d09394bc860f
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-09-08 16:43:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
+require('dotenv').config();
 const express = require('express');
 const { getCityInfo, getJobs } = require('./util');
-const app = express();
-require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const app = express();
 
 app.use(express.static('public'));
 
 app.get('/api/city/:city', async (req, res) => {
   try {
-    const cityInfo = await getCityInfo(req.params.city);
-    const jobs = await getJobs(req.params.city);
-    
+    const city = req.params.city;
+    const cityInfo = await getCityInfo(city);
+    const jobs = await getJobs(city);
+
     if (cityInfo || jobs) {
       res.json({ cityInfo, jobs });
     } else {
-      res.status(404).json({ error: 'Not found' });
+      res.status(404).json({ error: 'Data not found' });
     }
   } catch (error) {
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
+
